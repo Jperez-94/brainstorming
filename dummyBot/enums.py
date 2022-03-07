3a34b539848f6ee14e88173f5831bf5421152edc
Jperez-94 <jose94.perez.perez@gmail.com>
2022-03-07 23:38:04 +0100

    Add enums file

---
+++ dummyBot/enums.py
@@ -0,0 +1,17 @@
+
+class enum_tier():
+    challenger = 0
+    grandmaster = 1
+    master = 2
+    diamond = 3
+    platinum = 4
+    gold = 5
+    silver = 6
+    bronze = 7
+    iron = 8
+
+class enum_division():
+    division_I = 0
+    division_II = 1
+    division_III = 2
+    division_IV = 3
